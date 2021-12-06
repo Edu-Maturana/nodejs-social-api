e39896fe4428551d85154b9dd14d60f007c747d0
Eduardo Maturana <edu@Mac-mini-de-Eduardo.local>
2021-12-06 12:57:13 -0300

    add login and jwt

---
+++ controllers/auth.js
@@ -0,0 +1,52 @@
+const { response } = require("express");
+const bcrypt = require("bcryptjs");
+
+const User = require("../models/user");
+const { generateJWT } = require("../helpers/jwt");
+
+const login = async (req, res = response) => {
+  const { name, password } = req.body;
+  try {
+    // Verify if the user exists
+    const user = await User.findOne({ name });
+    if (!user) {
+      return res.status(400).json({
+        ok: false,
+        msg: "The data is incorrect",
+      });
+    }
+
+    // Verify if the user is active
+    if (!user.state) {
+      return res.status(400).json({
+        ok: false,
+        msg: "The data is incorrect",
+      });
+    }
+
+    // Verify password
+    const validPassword = bcrypt.compareSync(password, user.password);
+    if (!validPassword) {
+      return res.status(400).json({
+        ok: false,
+        msg: "The data is incorrect",
+      });
+    }
+
+    // Generate JWT
+    const token = await generateJWT(user.id);
+
+    res.json({
+      ok: true,
+      user,
+      token,
+    });
+  
+  } catch (error) {
+    console.log(error);
+    res.status(500).json({
+      ok: false,
+      msg: "Error in the server",
+    });
+  }
+};
