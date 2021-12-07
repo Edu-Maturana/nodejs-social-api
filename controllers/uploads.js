0e98d11abc32ccd9bf07181f9901951da95cd372
Eduardo Maturana <edu@Mac-mini-de-Eduardo.local>
2021-12-07 17:21:05 -0300

    change position of req.files requirement

--- controllers/uploads.js
+++ controllers/uploads.js
@@ -8,24 +8,23 @@ const User = require('../models/user');
 
 const changeAvatar = async (req, res = response) => {
     const {id} = req.params;
-    const {tempFilePath} = req.files.avatar;
-
+    
     if (!req.files) {
         return res.status(400).json({
             ok: false,
             msg: 'No files were uploaded'
         });
     }
-
+    
     const user = await User.findById(id);
-
+    
     if (!user) {
         return res.status(404).json({
             ok: false,
             msg: 'User not found'
         });
     }
-
+    
     // Verify if the user is the same that the one that is trying to change the avatar
     if (user.id !== req.user.id) {
         return res.status(401).json({
@@ -33,18 +32,19 @@ const changeAvatar = async (req, res = response) => {
             msg: 'User not authorized'
         });
     }
-
+    
     if (user.avatar && user.avatar != defaultImage) {
         const nameArr = user.avatar.split('/');
         const name = nameArr[ nameArr.length - 1 ];
         const [ public_id ] = name.split('.');
         await cloudinary.uploader.destroy( public_id );
     }
-
+    
+    const {tempFilePath} = req.files.avatar;
     const {secure_url} = await cloudinary.uploader.upload(tempFilePath);
-
+    
     user.avatar = secure_url;
-
+    
     await user.save();
 
     res.json({
