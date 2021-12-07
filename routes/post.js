60bb61ff094f132bed5911342287a2c7070f321b
Eduardo Maturana <edu@Mac-mini-de-Eduardo.local>
2021-12-06 21:39:33 -0300

    complete post router

--- routes/post.js
+++ routes/post.js
@@ -1,22 +1,40 @@
 const { Router } = require("express");
 const { check } = require("express-validator");
 
-const {validateFields, validateJWT} = require("../middlewares");
+const { validateFields, validateJWT } = require("../middlewares");
 
-const {createPost, readPosts} = require("../controllers/post");
+const {
+  createPost,
+  readPosts,
+  readPost,
+  updatePost,
+  deletePost,
+} = require("../controllers/post");
 const router = Router();
 
 router.get("/", readPosts);
+router.get("/:id", readPost);
 
 router.post(
-    "/",
+  "/",
+  [
+    check("content", "Content is required").not().isEmpty(),
+    validateFields,
+    validateJWT,
+  ],
+  createPost
+);
+
+router.put(
+    "/:id",
     [
         check("content", "Content is required").not().isEmpty(),
         validateFields,
-        validateJWT
+        validateJWT,
     ],
-    createPost
+    updatePost
 );
 
-module.exports = router;
+router.delete("/:id", validateJWT, deletePost);
 
+module.exports = router;
