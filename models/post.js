478be653ea1586d877d7f926689d11dc4a1ba57b
Eduardo Maturana <edu@Mac-mini-de-Eduardo.local>
2021-12-08 19:18:48 -0300

    delete img property in post model

--- models/post.js
+++ models/post.js
@@ -14,10 +14,6 @@ const PostSchema = new Schema({
         type: Schema.Types.ObjectId,
         ref: 'Comment'
     }],
-    img: {
-        type: String,
-        default: ''
-    },
     createdAt: {
         type: Date,
         default: Date.now
